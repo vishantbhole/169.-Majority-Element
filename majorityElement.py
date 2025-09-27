70fd6bf70e727151741bf3fef0a43ec58491e251
Vishant <44126013+vishantbhole@users.noreply.github.com>
2025-09-26 19:31:25 -0500

    Update majorityElement.py

--- majorityElement.py
+++ majorityElement.py
@@ -10,3 +10,8 @@ class Solution(object):
         for n in nums:
             if count == 0:
                 majority = n
+            if n == majority:
+                count += 1
+            else:
+                count -= 1
+        return majority
